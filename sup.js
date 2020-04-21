f1ccc8140c33377d256ad13ef0f410123a9231db
Greg Kogan <greg@Gregs-MBP-2.hsd1.md.comcast.net>
2020-04-20 21:51:40 -0400

    add URL decoder, text input support, and time delay,

--- sup.js
+++ sup.js
@@ -1,10 +1,8 @@
 /*
-How to use this script:
-1. Add a hidden form field for each query string parameter you’d like to capture.
-   The field’s name should match the key of the query string parameter.
-2. Include the script on the page(s) where you’d like to use it, and activate it in your JavaScript:
-   queryForm({reset: true});
-   The value {reset: true} is optional. Including it will clear (reset) parameter values after a form is submitted.
+1. Add a hidden form field for each URL parameter you’d like to capture.
+   The field’s name should match the key of the URL parameter.
+2. Include the script on the page(s) where you’d like to use it.
+   Change queryForm() to queryForm({reset: true}) to reset values upon form submisision.
 */
 
 var queryForm = function(settings){
@@ -16,13 +14,15 @@ var queryForm = function(settings){
     for (i in pairs) {
       var keyval = pairs[i].split("=");
       if (reset || sessionStorage.getItem(keyval[0]) === null) {
-        sessionStorage.setItem(keyval[0], keyval[1]);
+        sessionStorage.setItem(keyval[0], decodeURIComponent(keyval[1]));
       }
     }
   }
-  var hiddenFields = document.querySelectorAll("input[type=hidden]");
+  var hiddenFields = document.querySelectorAll("input[type=hidden], input[type=text]");
   for (var i=0; i<hiddenFields.length; i++) {
     var param = sessionStorage.getItem(hiddenFields[i].name);
     if (param) document.getElementsByName(hiddenFields[i].name)[0].value = param;
   }
 }
+
+setTimeout(function(){queryForm();}, 3000);
